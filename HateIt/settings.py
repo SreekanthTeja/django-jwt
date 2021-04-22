fe13d80a1c82b4bee6316026fdeaa2525b2e2f4f
SreekanthTeja <srikanthteja24@gmail.com>
2021-04-22 16:20:15 +0530

    postgree added

--- HateIt/settings.py
+++ HateIt/settings.py
@@ -67,11 +67,14 @@ WSGI_APPLICATION = 'HateIt.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hateit',
+        'USER':'teja',
+        'HOST':'127.0.0.1',
+        'PASSWORD':'rootroot',
+        'PORT':'5432'
     }
 }
 
